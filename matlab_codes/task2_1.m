da0a96fb299570b14ae5005b8044b87b692cfcdb
JennyL5 <Jennylin5399@hotmail.co.uk>
2019-04-05 05:23:36 +0100

    Completed task 2_1

--- matlab_codes/task2_1.m
+++ matlab_codes/task2_1.m
@@ -1,5 +1,4 @@
-%
-%
+
 function task2_1(Xtrain, Ytrain, Xtest, Ytest, Ks)
 % Input:
 %  Xtrain : M-by-D training data matrix (double)
@@ -10,35 +9,26 @@ function task2_1(Xtrain, Ytrain, Xtest, Ytest, Ks)
 
 num = size(Xtest,1);
 K=10;
-%disp(num);
 
 for k=1:size(Ks, 2)
     %Measure the time taken, and display it.
     tic
     Ypreds = run_knn_classifier(Xtrain, Ytrain, Xtest, Ks(k));
     time = toc;
-%     fprintf('Number of nearest neighbours: %d. \n',Ks(k));
-%     fprintf('Number of test samples: %d.\n',num);
-     fprintf('Time taken for k = %d at %.2fs.\n',Ks(k),time);
+    
+    fprintf('\nTime taken for k = %d at %.2fs.\n',Ks(k),time);
     
     %Get confusion matrix and accuracy for each k in Ks
     [cm, acc] = comp_confmat(Ytest, Ypreds(:,1),K);
-    acc = acc*100;
+
     %Save each confusion matrix
-    save(sprintf('task2_1_cm%d.mat',Ks(k)));
-    
+    %save(sprintf('task2_1_cm%d.mat',Ks(k)));
+
     %Sum of wrongly classified samples
     Nerrs = num-sum(diag(cm));
     %fprintf('\nNumber of wrongly classified test samples: %d\n', wrong);
     
     %display
-     NumNeighbours = sprintf('Number of K-nearest neighbours: %d, Number of test samples: %d, Number of wrongly classified test samples: %d, Accuracy: %d',Ks(k),num, Nerrs, acc);                   
-     disp(NumNeighbours);
-
-%   disp(k); % The number of nearest neighbours k
-%   disp(num); % The number of test samples size(Xtst,1),
-%   disp(Nerrs) % The number of wrongly classi?ed test samples  sum
-%   disp(acc) %Accuracy (i.e. correct classi?cation rate) acc
-
-    
+     fprintf('Number of K-nearest neighbours: %d. Number of test samples: %d. Number of wrongly classified test samples: %d, Accuracy: %.4fs.% \n',Ks(k),num, Nerrs, acc*100);                   
+     
 end
